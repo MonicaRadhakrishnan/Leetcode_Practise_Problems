c2a0a37d2327aea5e88d983d4b24cb05c2257c90
mradhakrish9 <mradhakrish9@dxc.com>
2023-06-08 18:55:27 +0530

    Commit #2

--- LeetCodeProblems/src/com/problems/practise/RunningSumof1DArray_1480.java
+++ LeetCodeProblems/src/com/problems/practise/RunningSumof1DArray_1480.java
@@ -1,10 +1,21 @@
 package com.problems.practise;
 
+import java.util.Arrays;
+
 public class RunningSumof1DArray_1480 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		RunningSumof1DArray_1480 rs = new RunningSumof1DArray_1480();
+		int[] nums = {1,2,3,4};
+		System.out.println(Arrays.toString(rs.runningSum(nums)));
 
 	}
-
+	  public int[] runningSum(int[] nums) {
+	        int[] resultSum = new int[nums.length];
+	        resultSum[0] = nums[0];
+	        for(int i =1 ; i < nums.length; i++){
+	            resultSum[i] = nums[i] + resultSum[i-1];
+	        }
+	        return resultSum;
+	    }
 }
